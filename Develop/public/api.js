26f9e9429d4923187a3eba976c430ad6a0b7a7d2
kevin cox <kevincox1983@gmail.com>
2020-05-18 13:32:14 -0500

    added morgan logger

--- Develop/public/api.js
+++ Develop/public/api.js
@@ -1,3 +1,4 @@
+const router = require("express").Router();
 const API = {
   async getLastWorkout() {
     let res;
@@ -42,3 +43,5 @@ const API = {
     return json;
   },
 };
+
+module.exports = router;
